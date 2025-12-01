44fd1ef2f4a18ea6ee872d71d823b8764a4a4f3e
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-12-01 21:02:37 +0100

    day2 part1 done

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -3,9 +3,54 @@ use std::env;
 use reqwest::blocking::Client;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let input = get_input()?;
 
+fn is_safe(report: &Vec<i32>) -> bool {
+    let mut i = 0;
+    if report[0] < report[1] {
+        loop {
+            if i == report.len() - 1 {
+                return true;
+            }
+            if report[i] > report[i + 1] {
+                return false;
+            }
+            if report[i + 1] - report[i] > 3 || report[i + 1] - report[i] < 1 {
+                return false;
+            }
+            i += 1;
+        }
+    } else {
+        loop {
+            if i == report.len() - 1 {
+                return true;
+            }
+            if report[i] < report[i + 1] {
+                return false;
+            } 
+            if report[i] - report[i + 1] > 3 || report[i] - report[i + 1] < 1 {
+                return false;
+            }
+            i += 1;
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut count = 0;
+    let input = get_input()?;
+    for report in &input {
+        let vec: Vec<i32> = report
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect();
+        if is_safe(&vec) {
+            count += 1;
+            println!("{:?} -> true", vec);
+        } else {
+            println!("{:?} -> false", vec);
+        }
+    }
+    println!("password = {count}");
     Ok(())
 }
 
