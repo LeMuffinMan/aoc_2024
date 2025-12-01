3f92855da09f510b560675e78e6596609fcddc5f
muffin <muffin@arch.man>
2025-12-01 23:23:27 +0100

    day2 part2 done

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -4,35 +4,39 @@ use reqwest::blocking::Client;
 use std::error::Error;
 
 
-fn is_safe(report: &Vec<i32>) -> bool {
+fn is_safe(report: &Vec<i32>, len: usize) -> usize {
     let mut i = 0;
     if report[0] < report[1] {
         loop {
-            if i == report.len() - 1 {
-                return true;
+            if i == len - 1 {
+                return len;
             }
-            if report[i] > report[i + 1] {
-                return false;
-            }
-            if report[i + 1] - report[i] > 3 || report[i + 1] - report[i] < 1 {
-                return false;
+            if report[i] >= report[i + 1] || report[i + 1] - report[i] > 3 || report[i + 1] - report[i] < 1 {
+                return i;
             }
             i += 1;
         }
     } else {
         loop {
-            if i == report.len() - 1 {
-                return true;
+            if i == len - 1 { 
+                return len;
             }
-            if report[i] < report[i + 1] {
-                return false;
+            if report[i] <= report[i + 1] || report[i] - report[i + 1] > 3 || report[i] - report[i + 1] < 1 { 
+                return i;
             } 
-            if report[i] - report[i + 1] > 3 || report[i] - report[i + 1] < 1 {
-                return false;
-            }
             i += 1;
         }
     }
+}
+
+fn is_tolerated(report: &Vec<i32>, index_to_remove: usize) -> bool {
+    let mut v = report.clone();
+    v.remove(index_to_remove);
+    let len = v.len();
+    if is_safe(&v, len) == len {
+        return true;
+    }
+    return false;
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -43,11 +47,22 @@ fn main() -> Result<(), Box<dyn Error>> {
             .split_whitespace()
             .map(|s| s.parse::<i32>().unwrap())
             .collect();
-        if is_safe(&vec) {
+        let len = vec.len();
+        let res = is_safe(&vec, len);
+        if res == len {
             count += 1;
-            println!("{:?} -> true", vec);
         } else {
-            println!("{:?} -> false", vec);
+            let mut i = 0;
+            loop {
+                if i == vec.len() {
+                    break;
+                }
+                if is_tolerated(&vec, i) {
+                    count += 1;
+                    break;
+                }
+                i += 1;
+            }
         }
     }
     println!("password = {count}");
