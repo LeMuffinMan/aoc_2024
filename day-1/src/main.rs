2592e2149f4db9e0fc37f6ca0b53e86e68fa6814
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-12-01 18:52:17 +0100

    gen_day.sh create

---
+++ day-1/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+use std::env;
+use reqwest::blocking::Client;
+use std::error::Error;
+
+mod dial;
+use dial::Dial;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = get_input()?;
+
+    Ok(())
+}
+
+fn get_input() -> Result<Vec<String>, Box<dyn Error>> {
+    dotenv::from_path("../.env").ok();
+    let session_cookie = env::var("AOC_SESSION")?;
+
+    let url = "https://adventofcode.com/2024/day/2/input";
+
+    let client = Client::new();
+    let response = client
+        .get(url)
+        .header("Cookie", format!("session={}", session_cookie))
+        .send()?
+        .text()?;
+
+    let lines: Vec<String> = response.lines().map(|l| l.to_string()).collect();
+
+    Ok(lines)
+}
+
